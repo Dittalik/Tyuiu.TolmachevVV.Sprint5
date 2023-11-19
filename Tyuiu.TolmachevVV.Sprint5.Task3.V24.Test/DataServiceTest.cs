a4262607eb23ab4cc498d276dda42a2f11a8e6ff
Vitaly Tolmachev <vitalhare@outlook.com>
2023-11-19 12:20:50 +0500

    Task3

---
+++ Tyuiu.TolmachevVV.Sprint5.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.TolmachevVV.Sprint5.Task3.V24.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint5.Task3.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        private static readonly DataService service1 = new DataService();
+
+        private static readonly string path = service1.SaveToFileTextData(3);
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fileinfo = new FileInfo(path);
+            Assert.IsTrue(fileinfo.Exists);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double data;
+            using (var binaryReader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                data = binaryReader.ReadDouble();
+            }
+            Assert.AreEqual(169.89, data);
+        }
+    }
+}
---
+++ Tyuiu.TolmachevVV.Sprint5.Task3.V24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.TolmachevVV.Sprint5.Task3.V24.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint5.Task3.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService service1 = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Толмачев. В. В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #5                                                                *");
+            Console.WriteLine("*Задание #3                                                               *");
+            Console.WriteLine("*Вариант #24                                                               *");
+            Console.WriteLine("*Выполнил: Толмачев Виталий Владимирович | ПКТб 23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = 3                                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string path = service1.SaveToFileTextData(3);
+            Console.WriteLine("File is written                                                           *");
+            Console.WriteLine("***************************************************************************");
+            double text;
+            using (var reader = new BinaryReader(File.Open(path, FileMode.Open), Encoding.UTF8))
+            {
+                text = reader.ReadDouble();
+            }
+            Console.WriteLine(text.ToString());
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}

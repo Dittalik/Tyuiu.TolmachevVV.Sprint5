f3678bea3ead51c807b27b105da0c4c0fe244b40
Vitaly Tolmachev <vitalhare@outlook.com>
2023-11-20 19:29:29 +0500

    Task7

---
+++ Tyuiu.TolmachevVV.Sprint5.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.TolmachevVV.Sprint5.Task7.V12.Lib
+{
+    public class DataService : ISprint5Task7V12
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string outputpath = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7V12.txt";
+            File.WriteAllText(outputpath, File.ReadAllText(path).ToUpper());
+            return outputpath;
+        }
+    }
+}
---
+++ Tyuiu.TolmachevVV.Sprint5.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.TolmachevVV.Sprint5.Task7.V12.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint5.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService service = new DataService();
+        private static readonly string InputPath = @"C:\DataSprint5\InPutDataFileTask7V12.txt";
+
+        [TestMethod]
+        public void FileExsistenceTest()
+        {
+            FileInfo fileInfoInput = new FileInfo(InputPath);
+            FileInfo fileInfoOutput = new FileInfo(service.LoadDataAndSave(InputPath));
+            Assert.IsTrue(fileInfoInput.Exists);
+            Assert.IsTrue(fileInfoOutput.Exists);
+        }
+
+        [TestMethod]
+        public void CalculationTest()
+        {
+            Assert.AreEqual(File.ReadAllText(InputPath).ToUpper(), File.ReadAllText(service.LoadDataAndSave(InputPath)));
+        }
+    }
+}

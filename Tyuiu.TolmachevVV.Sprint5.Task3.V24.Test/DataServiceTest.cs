c9a9855e8e24bf32a6e73f90b246e8035adf7b3a
Vitaly Tolmachev <vitalhare@outlook.com>
2023-11-21 14:04:17 +0500

    Task3

--- Tyuiu.TolmachevVV.Sprint5.Task3.V24.Test/DataServiceTest.cs
+++ Tyuiu.TolmachevVV.Sprint5.Task3.V24.Test/DataServiceTest.cs
@@ -13,21 +13,24 @@ namespace Tyuiu.TolmachevVV.Sprint5.Task3.V24.Test
         private static readonly string path = service1.SaveToFileTextData(3);
 
         [TestMethod]
-        public void TestMethod1()
+        public void FileExistence()
         {
             var fileinfo = new FileInfo(path);
             Assert.IsTrue(fileinfo.Exists);
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void DataTests()
         {
-            double data;
+            string strData;
+            double doubleData;
             using (var binaryReader = new BinaryReader(File.Open(path, FileMode.Open)))
             {
-                data = binaryReader.ReadDouble();
+                strData = binaryReader.ReadString();
+                doubleData = binaryReader.ReadDouble();
             }
-            Assert.AreEqual(169.89, data);
+            Assert.AreEqual("14-AE-47-E1-7A-3C-65-40", strData);
+            Assert.AreEqual(169.89, doubleData);
         }
     }
 }
